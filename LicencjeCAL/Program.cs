6a659ea7460cbf86734fde2e1c6e429ea019daea
mateu <mateu@DESKTOP-HM7G6VA>
2024-02-28 21:39:59 +0100

    Add project files.

---
+++ LicencjeCAL/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+void ExecuteAsAdmin(string fileName)
+{
+    Process proc = new Process();
+    proc.StartInfo.FileName = fileName;
+    proc.StartInfo.UseShellExecute = true;
+    proc.StartInfo.Verb = "runas";
+    proc.Start();
+}
+System.Diagnostics.Process[] proc = System.Diagnostics.Process.GetProcessesByName("mstsc");
+if (proc.Length > 0)
+{
+    proc[0].Kill();
+}
+Registry.LocalMachine.DeleteSubKeyTree(@"SOFTWARE\Microsoft\MSLicensing\HardwareID");
+Registry.LocalMachine.DeleteSubKeyTree(@"SOFTWARE\Microsoft\MSLicensing\Store");
+ExecuteAsAdmin("mstsc.exe");
+
+
+
